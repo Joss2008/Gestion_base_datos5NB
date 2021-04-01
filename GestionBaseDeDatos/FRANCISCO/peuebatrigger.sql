e8075b0f07f9bf3dea2a1ffa9ba4f0fb5fc6c457
Joss2008 <56269296+Joss2008@users.noreply.github.com>
2021-04-01 08:56:38 -0500

    Add files via upload

---
+++ GestionBaseDeDatos/FRANCISCO/peuebatrigger.sql
@@ -0,0 +1,53 @@
+﻿CREATE TABLE numeros(
+ numero bigint NOT NULL,
+ cuadrado bigint,
+ cubo bigint,
+ raiz2 real,
+ raiz3 real,
+ PRIMARY KEY (numero)
+);
+
+CREATE OR REPLACE FUNCTION proteger_datos() RETURNS TRIGGER AS
+$proteger_datos$
+ DECLARE
+ BEGIN
+
+ --
+ -- Esta funcion es usada para proteger datos en un tabla
+ -- No se permitira el borrado de filas si la usamos
+ -- en un disparador de tipo BEFORE / row-level
+ --
+ RETURN NULL;
+ END;
+$proteger_datos$ LANGUAGE plpgsql;
+
+CREATE TRIGGER proteger_datos BEFORE DELETE
+ ON numeros FOR EACH ROW
+ EXECUTE PROCEDURE proteger_datos();
+
+
+CREATE OR REPLACE FUNCTION rellenar_datos() RETURNS TRIGGER AS $rellenar_datos$
+ DECLARE
+ BEGIN
+
+ NEW.cuadrado := power(NEW.numero,2);
+ NEW.cubo := power(NEW.numero,3);
+ NEW.raiz2 := sqrt(NEW.numero);
+ NEW.raiz3 := cbrt(NEW.numero);
+ RETURN NEW;
+ END;
+$rellenar_datos$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER rellenar_datos BEFORE INSERT OR UPDATE
+ON numeros FOR EACH ROW
+EXECUTE PROCEDURE rellenar_datos();
+
+INSERT INTO numeros (numero) VALUES (2);
+INSERT INTO numeros (numero) VALUES (3);
+
+SELECT * from numeros;
+
+UPDATE numeros SET numero = 4 WHERE numero = 3;
+
+
